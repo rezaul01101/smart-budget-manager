9a3b865d02b0b2f3f6f409b7e12d36f62d8cea13
Rezaul Hoque <rezaul@supremesupports.com.au>
2025-12-06 15:30:26 +0600

    Prisma and Postgresql setup is done

---
+++ backend/prisma/prisma.config.ts
@@ -0,0 +1,11 @@
+import { defineConfig } from 'prisma/config'
+import config from "./../src/config";
+export default defineConfig({
+  schema: 'prisma/schema.prisma',
+  migrations: {
+    path: 'prisma/migrations',
+  },
+  datasource: {
+    url: config?.database_url as string,
+  },
+})
